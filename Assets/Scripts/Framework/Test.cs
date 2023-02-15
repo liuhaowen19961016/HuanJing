2605200428f374bb8a1e6a4aaa492919d6f75299
liuhaowen <597094538@qq.com>
2023-02-15 17:36:37 +0800

    no message

---
+++ Assets/Scripts/Framework/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private GameObjectPool bulletPool;
+
+    private GameObject bullet;
+
+    private void Awake()
+    {
+        bulletPool = ObjectPoolMgr.Ins.GetOrCreateGameObjectPool(Resources.Load<GameObject>("Prefabs/Bullet"), 10, transform);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            bullet = bulletPool.Get();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            bulletPool.Put(bullet);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            bulletPool.PutAll();
+        }
+    }
+}
